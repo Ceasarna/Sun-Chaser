d190b23b81eb108f6c230a7ca4360e1639982d60
Ceasarna <tommy.ekberg@gmail.com>
2022-05-04 08:27:14 +0200

    SettingsPage. Design klar

---
+++ lib/ManageAccountPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManageAccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('ManageAccountPage',style: TextStyle(fontSize: 60),)),
+    );
+  }
+}
--- lib/SettingsPage.dart
+++ lib/SettingsPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_applicationdemo/BottomNavPage.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'ManageAccountPage.dart';
 
 // Standard color of app
 Color _backgroundColor = const Color.fromARGB(255, 190, 146, 160);
@@ -109,23 +111,19 @@ class _SettingsPageState extends State<SettingsPage> {
                             print("Tapped single dollarSign");
                           },
                           child: Container(
-                            height: 75,
                             color: _colorContainerLow,
+                            height: 60,
+                            width: 75,
                             margin:
                                 const EdgeInsets.only(left: 20.0, right: 20.0),
-                            child: Expanded(
-                              flex:1,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: const <Widget>[
-                                  Icon(
-                                    Icons.attach_money,
-                                    size:50,
-                                    textDirection: TextDirection.ltr,
-                                    semanticLabel: 'Icon',
-                                  ),
-                                ],
-                              ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: const <Widget>[
+                                Icon(
+                                  Icons.attach_money,
+                                  size: 27,
+                                ),
+                              ],
                             ),
                           ),
                         ),
@@ -140,6 +138,8 @@ class _SettingsPageState extends State<SettingsPage> {
                           },
                           child: Container(
                             color: _colorContainerMedium,
+                            height: 60,
+                            width: 75,
                             margin:
                                 const EdgeInsets.only(left: 20.0, right: 20.0),
                             child: Row(
@@ -147,9 +147,11 @@ class _SettingsPageState extends State<SettingsPage> {
                               children: const <Widget>[
                                 Icon(
                                   Icons.attach_money,
+                                  size: 27,
                                 ),
                                 Icon(
                                   Icons.attach_money,
+                                  size: 27,
                                 ),
                               ],
                             ),
@@ -166,19 +168,24 @@ class _SettingsPageState extends State<SettingsPage> {
                           },
                           child: Container(
                             color: _colorContainerHigh,
+                            height: 60,
+                            width: 75,
                             margin:
                                 const EdgeInsets.only(left: 20.0, right: 20.0),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: const <Widget>[
                                 Icon(
                                   Icons.attach_money,
+                                  size: 27,
                                 ),
                                 Icon(
                                   Icons.attach_money,
+                                  size: 27,
                                 ),
                                 Icon(
                                   Icons.attach_money,
+                                  size: 27,
                                 ),
                               ],
                             ),
@@ -190,6 +197,128 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
               ],
             ),
+            CustomSettingsSection(
+                child: Column(children: [
+              SizedBox(
+                width: double.infinity,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 25.0, top: 10),
+                  child: const Text(
+                    'Account',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BottomNavPage()),
+                          );
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 15.0, top: 10),
+                          color: _backgroundColor,
+                          height: 60,
+                          width: 175,
+                          child: const Center(
+                            child: Text(
+                              'Manage account',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontFamily: 'Sacramento',
+                                color: Colors.black,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(2, 2),
+                                    blurRadius: 12.5,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BottomNavPage()),
+                          );
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 15.0, top: 10),
+                          color: _backgroundColor,
+                          height: 60,
+                          width: 175,
+                          child: const Center(
+                            child: Text(
+                              'Leave feedback',
+                              style: TextStyle(
+                                fontSize: 32,
+                                fontFamily: 'Sacramento',
+                                color: Colors.black,
+                                shadows: <Shadow>[
+                                  Shadow(
+                                    offset: Offset(2, 2),
+                                    blurRadius: 12.5,
+                                    color: Color.fromARGB(255, 0, 0, 0),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BottomNavPage()),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 10, right: 15),
+                        color: _backgroundColor,
+                        height: 60,
+                        width: 175,
+                        child: const Center(
+                          child: Text(
+                            'Log out',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontFamily: 'Sacramento',
+                              color: Colors.black,
+                              shadows: <Shadow>[
+                                Shadow(
+                                  offset: Offset(2, 2),
+                                  blurRadius: 12.5,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ]))
           ],
         ));
   }
