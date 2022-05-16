83e14e1d5b7f33f1bc757138d00775255378f03f
maze6343 <maze6343@student.su.se>
2022-05-16 11:36:25 +0200

    google_markers (#6)
    Reviewed-on: https://gitea.dsv.su.se/maze6343/pvt-project-google-maps/pulls/6

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -15,6 +16,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FlutterNativeSplashWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
