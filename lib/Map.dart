c0419bca8b61d86e8eff5af0ac72e3d0f4572b61
maze6343 <maze6343@student.su.se>
2022-05-18 20:53:07 +0200

    added information on bottom sheet

--- lib/Map.dart
+++ lib/Map.dart
@@ -102,113 +102,150 @@ class MapState extends State<Map> {
     super.initState();
   }
 
+  void createBottomSheet() {
+    Scaffold.of(context).showBottomSheet<void>(
+              ((context) {
+                return Container(
+                  height: 420,
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: const <Widget>[
+                        /*const Text('BottomSheet'),
+                        ElevatedButton(
+                          child: const Text('Close BottomSheet'),
+                          onPressed: () {Navigator.pop(context);})*/
+                        Image(image: AssetImage('assets/images/bild.png'))
+                          
+                      ],
+                    )
+                    ),
+                );
+              })
+            );
+  }
+
   intilize() {
     Marker marker_1;
     //for(var marker in markers) {
       marker_1 = Marker(
-        markerId: MarkerId('id_1'),
-        position: LatLng(59.320671571444514, 18.055854162299937),
-        infoWindow: InfoWindow(
+        markerId: const MarkerId('id_1'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.320671571444514, 18.055854162299937),
+        infoWindow: const InfoWindow(
           title: 'Münchenbryggeriet Beer Garden',
-          onTap: () => setState(() {}),
+         
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_2 = const Marker(
-        markerId: MarkerId('id_2'),
-        position: LatLng(59.33115735285231, 18.074432570090742),
-        infoWindow: InfoWindow(
+      Marker marker_2 = Marker(
+        markerId: const MarkerId('id_2'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.33115735285231, 18.074432570090742),
+        infoWindow: const InfoWindow(
           title: 'Le Hibou',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_3 = const Marker(
-        markerId: MarkerId('id_3'),
-        position: LatLng(59.3315552932853, 18.092751076985277),
-        infoWindow: InfoWindow(
+      Marker marker_3 = Marker(
+        markerId: const MarkerId('id_3'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.3315552932853, 18.092751076985277),
+        infoWindow: const InfoWindow(
           title: 'Strandbryggan',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_4 = const Marker(
-        markerId: MarkerId('id_4'),
-        position: LatLng(59.33632582609118, 18.072980646196587),
-        infoWindow: InfoWindow(
+      Marker marker_4 = Marker(
+        markerId: const MarkerId('id_4'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.33632582609118, 18.072980646196587),
+        infoWindow: const InfoWindow(
           title: 'Stureplan 1',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_5 = const Marker(
-        markerId: MarkerId('id_5'),
-        position: LatLng(59.3240158318325, 18.070690101341437),
-        infoWindow: InfoWindow(
+      Marker marker_5 = Marker(
+        markerId: const MarkerId('id_5'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.3240158318325, 18.070690101341437),
+        infoWindow: const InfoWindow(
           title: 'Bågspännaren Bar & Cafe',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_6 = const Marker(
-        markerId: MarkerId('id_6'),
-        position: LatLng(59.31905195030728, 18.075349015415547),
-        infoWindow: InfoWindow(
+      Marker marker_6 = Marker(
+        markerId: const MarkerId('id_6'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.31905195030728, 18.075349015415547),
+        infoWindow: const InfoWindow(
           title: 'Mosebacketerrassen',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_7 = const Marker(
-        markerId: MarkerId('id_7'),
-        position: LatLng(59.31583756143469, 18.072591381467536),
-        infoWindow: InfoWindow(
+      Marker marker_7 = Marker(
+        markerId: const MarkerId('id_7'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.31583756143469, 18.072591381467536),
+        infoWindow: const InfoWindow(
           title: 'Snaps Bar & Bistro',
           snippet: 'Uteservering',
         )
         );
       
-      Marker marker_8 = const Marker(
-        markerId: MarkerId('id_8'),
-        position: LatLng(59.315129508641505, 18.074243159987006),
-        infoWindow: InfoWindow(
+      Marker marker_8 = Marker(
+        markerId: const MarkerId('id_8'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.315129508641505, 18.074243159987006),
+        infoWindow: const InfoWindow(
           title: 'Kvarnen',
           snippet: 'Uteservering',
         )
         );
       
-      Marker marker_9 = const Marker(
-        markerId: MarkerId('id_9'),
-        position: LatLng(59.31533181094423, 18.070972638518455),
-        infoWindow: InfoWindow(
+      Marker marker_9 = Marker(
+        markerId: const MarkerId('id_9'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.31533181094423, 18.070972638518455),
+        infoWindow: const InfoWindow(
           title: 'Neverland Pub & Restaurang',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_10 = const Marker(
-        markerId: MarkerId('id_10'),
-        position: LatLng(59.31578389646754, 18.071146819010995),
-        infoWindow: InfoWindow(
+      Marker marker_10 = Marker(
+        markerId: const MarkerId('id_10'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.31578389646754, 18.071146819010995),
+        infoWindow: const InfoWindow(
           title: 'Baras Imperium',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_11 = const Marker(
-        markerId: MarkerId('id_11'),
-        position: LatLng(59.31549103673382, 18.035425964557245),
-        infoWindow: InfoWindow(
+      Marker marker_11 = Marker(
+        markerId: const MarkerId('id_11'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.31549103673382, 18.035425964557245),
+        infoWindow: const InfoWindow(
           title: 'YUC Tanto',
           snippet: 'Uteservering',
         )
         );
 
-      Marker marker_12 = const Marker(
-        markerId: MarkerId('id_12'),
-        position: LatLng(59.314826329005506, 18.03317611771755),
-        infoWindow: InfoWindow(
+      Marker marker_12 = Marker(
+        markerId: const MarkerId('id_12'),
+        onTap: createBottomSheet,
+        position: const LatLng(59.314826329005506, 18.03317611771755),
+        infoWindow: const InfoWindow(
           title: 'Loopen',
           snippet: 'Uteservering',
         )
@@ -302,8 +339,8 @@ class MapState extends State<Map> {
            _controller.complete(controller);
            },
           ),
-          ElevatedButton(onPressed: () {} //_handelPressButton
-          ,child: const Text("Search Placses"))
+         // ElevatedButton(onPressed: () {} //_handelPressButton
+        //  ,child: const Text("Search Placses"))
         ],
       )
     );
