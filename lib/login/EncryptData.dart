fec6bef35f2f50babeca7bac43598bb7dfaf47ae
stjertnylle <maen0574@student.su.se>
2022-05-30 11:00:30 +0200

    Markers showing depending on camera location, encrypting passwords

--- lib/login/EncryptData.dart
+++ lib/login/EncryptData.dart
@@ -4,22 +4,13 @@ class EncryptData{
 //for AES Algorithms
 
   static Encrypted? encrypted;
-  static var decrypted;
 
 
   static encryptAES(plainText){
-    final key = Key.fromUtf8('my 32 length key................');
+    final key = Key.fromUtf8('my32lengthsupersecretnooneknows1');
     final iv = IV.fromLength(16);
     final encrypter = Encrypter(AES(key));
     encrypted = encrypter.encrypt(plainText, iv: iv);
     return encrypted!.base64;
-  }
-
-  static decryptAES(plainText){
-    final key = Key.fromUtf8('my 32 length key................');
-    final iv = IV.fromLength(16);
-    final encrypter = Encrypter(AES(key));
-    decrypted = encrypter.decrypt(encrypted!, iv: iv);
-    return decrypted;
   }
 }
